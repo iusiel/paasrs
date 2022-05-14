45300c930adccd38d3d28333d751936c78b99bde
Iusiel <iusiel@outlook.com>
2022-05-14 08:55:33 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# PAASRS (Poor Attempt At Spaced Repetition Software)
+
+My attempt to create a spaced repetition software. I will be using Laravel 8 for this in order to support PHP 7.3 and above.
+
+## Installation
+// add later
+
+## How to use
+// add later
---
+++ resources/js/app.js
@@ -0,0 +1,15 @@
+require('./bootstrap');
+import('./../plugins/prism/prism.js');
+
+// import { createApp } from 'vue';
+// import Test from './components/Test.vue';
+// createApp({
+//     data() {
+//       return {
+//         // count: 0
+//       }
+//     },
+//     components: {
+//         Test
+//     }
+// }).mount('#app')
