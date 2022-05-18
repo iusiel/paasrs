cd5181289bafdfca0b6d94344ebbbbe66047f800
Iusiel <iusiel@outlook.com>
2022-05-18 21:34:48 +0800

    Create deck test

---
+++ tests/Feature/DeckCreateTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Support\Str;
+use Tests\TestCase;
+
+class DeckCreateTest extends TestCase
+{
+    use DatabaseTransactions;
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_function()
+    {
+        $response = $this->withHeader('X-Requested-With', 'XMLHttpRequest')->post('/decks', [
+            'name' => Str::random(20),
+        ]);
+
+        $response->assertJson([
+            'message' => 'Deck created successfully.'
+        ]);
+    }
+}
